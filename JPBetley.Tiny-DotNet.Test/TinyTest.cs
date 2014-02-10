1607e5d81c60ad7438605ccb2ea7d49bb0b596b0
John Betley <jpbetley@gmail.com>
2014-02-10 09:02:15 -0500

    Initial Commit

---
+++ JPBetley.Tiny-DotNet.Test/TinyTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JPBetley.Tiny_DotNet.Test
+{
+    [TestClass]
+    public class TinyTest
+    {
+        private Tiny tiny;
+        [TestInitialize]
+        public void Initialize()
+        {
+            this.tiny = new Tiny("5SX0TEjkR1mLOw8Gvq2VyJxIFhgCAYidrclDWaM3so9bfzZpuUenKtP74QNH6B");
+        }
+
+        [TestMethod]
+        public void TestToTiny()
+        {
+            var converted = this.tiny.To(5);
+            Assert.AreEqual("E", converted);
+        }
+
+        [TestMethod]
+        public void TestFromTiny()
+        {
+            var reversed = this.tiny.From("E");
+            Assert.AreEqual(5, reversed);
+        }
+
+        [TestMethod]
+        public void TestReversingRandomInt()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                var translated = this.tiny.From(this.tiny.To(i));
+                Assert.AreEqual(i, translated);
+            }
+        }
+    }
+}
---
+++ JPBetley.Tiny-DotNet/Tiny.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JPBetley.Tiny_DotNet
+{
+    public class Tiny
+    {
+        private string key;
+
+        public Tiny(string key)
+        {
+            this.key = key;
+        }
+
+        public string To(int id)
+        {
+            string key = this.key;
+            id = Math.Abs(id);
+
+            StringBuilder builder = new StringBuilder();
+            int radix = key.Length;
+
+            do
+            {
+                int keyIndex = id % radix;
+                builder.Insert(0, key.ElementAt(keyIndex));
+                id = (id - keyIndex) / radix;
+            } while (id != 0);
+
+            return builder.ToString();
+        }
+
+        public int From(string hex)
+        {
+            string key = this.key;
+            int radix = key.Length;
+            int length = hex.Length;
+            int id = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                id += key.IndexOf(hex.ElementAt(i)) * (int)Math.Pow(radix, (length - i - 1));
+            }
+
+            return id;
+        }
+    }
+}
